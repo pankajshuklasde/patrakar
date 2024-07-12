1710b6faed5c6d52e679c4036412e85416a26abb
pankajshuklasde <148037957+pankajshuklasde@users.noreply.github.com>
2024-07-12 18:48:34 +0530

    topic options

--- src/main/java/com/patrakar/patrakar/model/repository/Topic.java
+++ src/main/java/com/patrakar/patrakar/model/repository/Topic.java
@@ -2,9 +2,11 @@ package com.patrakar.patrakar.model.repository;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Unwrapped;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,8 +16,11 @@ import java.util.List;
 public class Topic {
     @Id
     private String id;
+    @NonNull
     private String mainTopic;
+    @NonNull
     private List<String> subTopics;
+    @NonNull
     private List<String> visitedLinks;
     @CreatedDate
     private LocalDateTime createdAt;
